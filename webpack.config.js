1087ef9372bf26423b21da49cd4017a654802162
Yoshida Tsubasa <71028859+T-Yoshida00112@users.noreply.github.com>
2021-10-21 21:41:30 +0900

    Use Asset Modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,46 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/javascripts/main.js',
+    entry: {
+        main:'./src/javascripts/main.js',
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'javascripts/main.js',
     },
     module: {
         rules: [{
-            test: /\.css/,
-            use: [{
-                    loader: MiniCssExtractPlugin.loader,
+                test: /\.css/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpg)/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
                 },
-                {
-                    loader: 'css-loader',
-                },
-            ],
-        }, ],
+                use: [
+                    // {
+                    //     loader: 'file-loader',
+                    //     options: {
+                    //         esModule: false,
+                    //         name: 'images/[name].[ext]',
+                    //     },
+                    // },
+                ],
+            },
+        ],
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -29,5 +50,5 @@ module.exports = {
             template: './src/templates/index.html',
         }),
         new CleanWebpackPlugin(),
-    ]
-}
+    ],
+};
